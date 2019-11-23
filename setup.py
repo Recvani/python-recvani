68907ac813fd0c1ed5886f78a3d87781cdf384cc
anshumankumar <anshu266man@gmail.com>
2019-11-24 02:20:43 +0530

    Adding the setup config

--- setup.py
+++ setup.py
@@ -16,8 +16,9 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    install_requires=['requests']
 )
