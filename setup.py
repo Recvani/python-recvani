0004f91c9d9370407072156ae1d305df59bd7026
anshumankumar <anshu266man@gmail.com>
2020-01-27 17:57:39 +0530

    Updating the readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="recvani",
-    version="0.0.6",
+    version="0.0.7",
     author="Anshuman kumar",
     author_email="anshuman@recvani.com",
     description="The client side python apis for making integration with recvani serveers",
